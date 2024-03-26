c8f1d5394783abede53157ead44ef06e4402147f
EsdrasMukuku <esdrasbam0@gmail.com>
2024-03-26 17:49:38 -0500

    finished empty search state

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// 6ad11fff
+fetch("http://www.omdbapi.com/?apikey=6ad11fff&s=superman")
+    .then(res => res.json())
+    .then(data =>  console.log(data.Search.forEach(element => {
+       console.log(element.Title) 
+    })))
+        
+    
 
+
+console.log("movie");
