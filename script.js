92c187e7a031d5aa4fcb5c089801ace7fda90f5a
EsdrasMukuku <esdrasbam0@gmail.com>
2024-03-29 21:29:44 -0500

    almost

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // 6ad11fff
-fetch("http://www.omdbapi.com/?apikey=6ad11fff&s=superman")
+const movieInput = document.getElementById('input');
+const searchBtn = document.getElementById('search');
+const moviesChoices = document.querySelector(".empty");
+
+
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    moviesChoices.style.color = "white";
+    moviesChoices.innerHTML = ''
+    moviesChoices.innerHTML += `<h1>${movieInput.value}</h1>`
+});
+
+
+fetch("http://www.omdbapi.com/?apikey=6ad11fff&s=avengers")
     .then(res => res.json())
-    .then(data =>  console.log(data.Search.forEach(element => {
-       console.log(element.Title) 
-    })))
-        
-    
+    .then(data => {
+        const fetchPromises = data.Search.map(element =>
+            fetch(`http://www.omdbapi.com/?apikey=6ad11fff&t=${element.Title}`)
+                .then(res => res.json())
+        );
+        return Promise.all(fetchPromises);
+    })
+    .then(movies => console.log(movies));
 
 
-console.log("movie");
